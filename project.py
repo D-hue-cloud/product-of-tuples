8b1fff089efa886aab2edac60632969b1e0af17a
D-hue-cloud <dinugaratnayake@gmail.com>
2026-05-10 17:37:01 +0100

    Add files via upload

---
+++ project.py
@@ -0,0 +1,15 @@
+tup1=(4,3,2,2,-1,18)
+product=1
+
+for num in tup1:
+    product*=num
+
+print(f"The product of tup1 is: ",{product})
+
+tup2=(2,4,8,8,3,2,9)
+product_of_2=1
+
+for num2 in tup2:
+    product*=num2
+
+print(f"The product of tup2 is: ", {product})
